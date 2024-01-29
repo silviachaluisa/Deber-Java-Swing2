4788c8e1de2305500db4293b743353dfad436433
silviachaluisa <veronica-14-1998@hotmail.com>
2024-01-29 17:04:41 -0500

    Main1

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main (String[] args){
+        JFrame frame=new JFrame("Bienvenido");
+        frame.setContentPane(new Form1().Menu);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+
+        frame.setVisible(true);
+
+
+
 
     }
 }
